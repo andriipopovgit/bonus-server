2913cf072ccfe9c3d7ed5488b1b4b818679c3c88
Андрій Попов <popov@kpnu.km.ua>
2022-08-24 17:34:19 +0300

    new feuteres

--- routes/index.js
+++ routes/index.js
@@ -9,21 +9,25 @@ router.get('/', (req, res, next) => {
 
 router.get('/customer/:code', async (req, res) => {
   const customer = await Customer.findOne({ code: req.params.code });
-  res.send(customer);
+  if (!customer) res.send({});
+  else res.send(customer);
 });
 
 router.post('/customer/:code/add-points', async (req, res) => {
   const customer = await Customer.findOne({ code: req.params.code });
-  console.log(customer);
-  console.log(req.body.points);
-  customer.points += req.body.points;
-  await customer.save();
-  res.send(customer);
+  if (customer) {
+    customer.points += req.body.points;
+    await customer.save();
+    res.send(customer);
+  } else {
+    res.send({});
+  }
 });
 
 router.post('/create-customer', async (req, res) => {
+  // console.log(req.body);
   const customer = await Customer.create({ ...req.body, points: 0 });
-  // const customer = await Customer.create({ name: 'Lyso Form', code: '8004450000117', points: 0});
+  // const customer = await Customer.create({ name: 'Wonderland woods', code: '667548099141', points: 0});
   res.send(customer);
 });
 
